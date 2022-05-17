ab11074bcb9ccdcaa2c974a86fc41e8847cf1007
Sabina Rafique <sabina.r@hotmail.co.uk>
2022-05-17 23:52:40 +0100

    MVP completed

--- chocolate_start_point/src/main/java/com/bnta/chocolate/controller/ChocolateController.java
+++ chocolate_start_point/src/main/java/com/bnta/chocolate/controller/ChocolateController.java
@@ -1,9 +1,16 @@
 package com.bnta.chocolate.controller;
 
+import com.bnta.chocolate.models.Chocolate;
 import com.bnta.chocolate.repositories.ChocolateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -12,5 +19,19 @@ public class ChocolateController {
 
     @Autowired
     ChocolateRepository chocolateRepository;
+
+    // INDEX
+    // GET request on localhost:8080/chocolates
+    @GetMapping
+    public ResponseEntity<List<Chocolate>> getAllChocolatesAndFilters(
+            @RequestParam(required = false, name = "cocoaPercentage") Integer cocoaPercentage
+    ) {
+        if (cocoaPercentage != null) {
+            return new ResponseEntity<>(
+                    chocolateRepository.findChocolateByCocoaPercentageGreaterThan(cocoaPercentage), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(chocolateRepository.findAll(), HttpStatus.OK);
+
+    }
 
 }
--- chocolate_start_point/src/main/java/com/bnta/chocolate/controller/EstateController.java
+++ chocolate_start_point/src/main/java/com/bnta/chocolate/controller/EstateController.java
@@ -1,9 +1,16 @@
 package com.bnta.chocolate.controller;
 
+import com.bnta.chocolate.models.Estate;
 import com.bnta.chocolate.repositories.EstateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("estates") // localhost:8080/estates
@@ -12,6 +19,10 @@ public class EstateController {
     @Autowired
     EstateRepository estateRepository;
 
-
+    // INDEX
+    // GET request on localhost:8080/estates
+    public ResponseEntity<List<Estate>> getAllEstates(){
+        return new ResponseEntity<>(estateRepository.findAll(), HttpStatus.OK);
+    }
 
 }
